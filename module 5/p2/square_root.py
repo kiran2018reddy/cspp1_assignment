6020fd8833ac10d42f2f4f8f175479a0529e83e3
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-08-05 16:23:13 -0700

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- module 5/p2/square_root.py
+++ module 5/p2/square_root.py
@@ -1,21 +1,16 @@
-# Write a python program to find the square root of the given number
-# using approximation method
-
-# testcase 1
-# input: 25
-# output: 4.999999999999998
-
-# testcase 2
-# input: 49
-# output: 6.999999999999991
-
+'''This program evaluates the square root of a number.'''
 def main():
-	s = raw_input()
-	# epsilon and step are initialized
-	# don't change these values
-	epsilon = 0.01
-	step = 0.1
-	# your code starts here
-
-if __name__== "__main__":
-	main()
+    '''Main function.'''
+    num_1 = int(input())
+    epsilon = 0.01
+    step = 0.1
+    guess = 0
+    while abs(guess**2-num_1) >= epsilon:
+        guess += step
+    if abs(guess**2-num_1) >= epsilon:
+        print("Failed on square root of", num_1)
+    else:
+        print(guess)
+if __name__ == "__main__":
+    main()
+    
