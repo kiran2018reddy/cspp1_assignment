031532abf362cbc004ab20479aeeb2483a73f6b6
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-08-06 18:19:01 -0700

    " -> 10 of 10 passed. pylint: 6.84/10 "

--- m7/Functions - Assignment-1/assignment1.py
+++ m7/Functions - Assignment-1/assignment1.py
@@ -1,46 +1,20 @@
-# Functions | Assignment-1 - Paying Debt off in a Year
-
-# Write a program to calculate the credit card balance after one year if a person only pays the minimum monthly payment required by the
-# credit card company each month.
-
-# The following variables contain values as described below:
-# balance - the outstanding balance on the credit card
-# annualInterestRate - annual interest rate as a decimal
-# monthlyPaymentRate - minimum monthly payment rate as a decimal
-
-# For each month, calculate statements on the monthly payment and remaining balance. At the end of 12 months, print out the remaining
-# balance. Be sure to print out no more than two decimal digits of accuracy - so print
-
-# Remaining balance: 813.41
-# instead of
-# Remaining balance: 813.4141998135
-
-# So your program only prints out one thing: the remaining balance at the end of the year in the format:
-# Remaining balance: 4784.0
-
-# A summary of the required math is found below:
-# Monthly interest rate= (Annual interest rate) / 12.0
-# Minimum monthly payment = (Minimum monthly payment rate) x (Previous balance)
-# Monthly unpaid balance = (Previous balance) - (Minimum monthly payment)
-# Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
-
-
-def payingdebtoffinayear(balance, annualInterestRate, monthlyPaymentRate):
+"""balance"""
+def paying_debt_offinayear(balance, a_i_r, monthlypaymentrate):
     temp = balance
     months = 12
     while months >= 1:
-        monthlyinterestrate = annualInterestRate / 12.0
-        minimummonthlypayment = monthlypaymentrate * temp
-        monthlyunpaidbalance = temp - minimummonthlypayment
-        var = monthlyinterestrate*monthlyunpaidbalance
-        updatedbalanceeachmonth = monthlyunpaidbalance + (var)
-        temp = updatedbalanceeachmonth
+        m_i_r = a_i_r / 12.0
+        m_m_p = monthlypaymentrate * temp
+        m_u_b = temp - m_m_p
+        var = m_i_r*m_u_b
+        u_b_e_m = m_u_b + (var)
+        temp = u_b_e_m
         months = months - 1
     return(round(temp, 2))
 def main():
     data = input()
     data = data.split(' ')
     data = list(map(float, data))
-    print("Remaining balance:",payingdebtoffinayear(data[0],data[1],data[2]))
-if __name__== "__main__":
+    print("Remaining balance:",paying_debt_offinayear(data[0],data[1],data[2]))
+if __name__ == "__main__":
     main()
