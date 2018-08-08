d35352650e35cba2ed4336c18af90291af1c4e35
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-08-07 21:35:25 -0700

    hlooo

---
+++ m8/assignment2.py
@@ -0,0 +1,15 @@
+'''Sum of digits using recursion.'''
+def sumofdigits(n_1):
+    '''
+    n is positive Integer
+    returns: a positive integer, the sum of digits of n.
+    '''
+    if n_1 == 0:
+        return 0
+    return n_1%10+sumofdigits(n_1//10)
+def main():
+    '''Main Function.'''
+    a_1 = input()
+    print(sumofdigits(int(a_1)))
+if __name__ == "__main__":
+    main()
