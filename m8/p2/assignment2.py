433d16cfeca84ca0a25db95afc524d4d47c705c1
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-08-07 18:52:49 -0700

    seven

---
+++ m8/p2/assignment2.py
@@ -0,0 +1,18 @@
+# Exercise: Assignment-2
+# Write a Python function, sumofdigits, that takes in one number and returns the sum of digits of given number.
+
+# This function takes in one number and returns one number.
+
+
+def sumofdigits(n):
+    '''sum'''
+    while n > 0:
+        r = n % 10
+        return r+sumofdigits(n//10)
+    return 0
+def main():
+    '''sum'''
+    a = input()
+    print(sumofdigits(int(a)))  
+if __name__== "__main__":
+    main()
