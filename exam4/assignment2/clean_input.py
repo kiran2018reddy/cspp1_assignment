97448210b41f638d870c8d09f679f31252581683
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-08-25 11:31:56 -0700

    " -> 0 of 3 passed. pylint: 7.14/10 "

--- exam4/assignment2/clean_input.py
+++ exam4/assignment2/clean_input.py
@@ -4,7 +4,7 @@ alphabets in both upper and lower case and numbers.
 '''
 
 def clean_string(string):
-    " ".join([i for i in s if i.isalpha()])
+    " ".join([i for i in string if i.isalpha()])
 
 def main():
     string = input()
