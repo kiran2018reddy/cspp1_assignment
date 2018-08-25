42aa0fc83df8e3ff6dd8387d47432ba7a824b010
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-08-25 12:10:53 -0700

    " -> 0 of 3 passed. pylint: 0/0 "

--- exam4/assignment2/clean_input.py
+++ exam4/assignment2/clean_input.py
@@ -4,8 +4,7 @@ alphabets in both upper and lower case and numbers.
 '''
 
 def clean_string(string):
-    return " ".join([i for i in string if ord(i) in range(97, 123) \
-                    or ord(i) in range(65, 91)])
+    return " ".join([i for i in string if ord(i) in range(97, 123) or ord(i) in range(65, 91)] or ord(i) in range(48,58)])
 
 def main():
     string = input()
@@ -13,4 +12,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
